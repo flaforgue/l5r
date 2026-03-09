f822d6843f207ed08015311ef93f730238d13477
Florian Laforgue <florian.laforgue@gmail.com>
2026-03-09 11:57:59 +0100

    Fix vite config for github pages refresh

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,21 @@
 import path from "path"
+import fs from 'node:fs'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
+  plugins: [
+    react(),
+    tailwindcss(),
+    {
+      name: 'index',
+      closeBundle() {
+        fs.copyFileSync('dist/index.html', 'dist/404.html')
+      },
+    },
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
